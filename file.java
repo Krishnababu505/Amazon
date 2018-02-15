cebc17965ad0b6dafa8de2945cd6edf15bc17012
krishna <krish.rk505@gmail.com>
2018-02-15 13:13:25 +0530

    print details changed

--- file.java
+++ file.java
@@ -2,6 +2,8 @@ public class file
 {
 public static void main(String args[])
 {  
-     System.out.println("Hello Java");  
+     System.out.println("Hello welcome to Java world");
+
+     System.out.println("welcome Home");  
     }  
 }  
